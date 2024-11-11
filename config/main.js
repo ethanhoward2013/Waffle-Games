f4ff47a8de5910475d2191e851e9090ef70a68fb
fartsucker9000 <ethanrhoward2013@gmail.com>
2024-11-11 02:34:22 +0000

    Pending changes exported from your codespace

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "Waffle Games Unblocked"; // Change this to change the name of your website.
+var subtext = "Best unblocked Games!"; // set the subtext
 
 // more settings in main.css
 
